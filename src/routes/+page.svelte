a243a9d3dc3a4781570a8b8cdcb6894f9ba4a607
minejerik <jaanerikf@gmail.com>
2025-10-06 15:09:53 -0400

    move navbar to seperate file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
-  import "../app.css";
+	import HCnav from '$lib/HCnav.svelte';
+	import '../app.css';
 </script>
+
+<HCnav></HCnav>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button class="btn btn-primary">Button</button>
