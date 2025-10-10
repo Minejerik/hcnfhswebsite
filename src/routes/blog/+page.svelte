56c5d19788ba0a7a33c415421ab3e1c513037700
Minejerik <jaanerikf@gmail.com>
2025-10-10 17:27:46 -0400

    Working on blogpost page, and general cleanups

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,15 +4,15 @@
 	import '../../app.css';
 	import { pb } from '$lib/pocketbase';
 
-    let blogList: any[] = [];
+	let blogList: any[] = [];
 
-    onMount(async () => {
-        const blogListPB = await pb.collection("posts").getFullList({
-            sort: "-took_place"
-        })
+	onMount(async () => {
+		const blogListPB = await pb.collection('posts').getFullList({
+			sort: '-took_place'
+		});
 
-        blogList = blogListPB
-    })
+		blogList = blogListPB;
+	});
 </script>
 
 <HCnav></HCnav>
@@ -21,32 +21,31 @@
 	<title>HC@NFHS | Blog</title>
 </svelte:head>
 
-<div class="min-h-screen bg-base-200 pt-25 pl-15 pr-15">
-    {#each blogList as post}
+<div class="min-h-screen bg-base-200 pt-25 pr-15 pl-15">
 	<div class="flex flex-wrap gap-4">
 		<div class="bg-base-100">
-			<div class="card h-50 w-96 min-w-50 bg-base-100 shadow-sm outline-solid">
-				<a href={`blog_post/${post.id}`} class="h-50 bg-base-100">
-					<figure>
-						<img
-							src={pb.files.getURL(post, post.images[0], {thumb: "400x0"})}
-							alt="For post"
-						/>
-					</figure>
-					<div class="card-body bg-base-300">
-						<h2 class="card-title">{post.title}</h2>
-						<p>{@html post.description}</p>
-						<hr />
-						<div class="card-actions">
-                                <span>Written by: {post.author}</span>
-                                <div class="text-right">
-                                    <span>{post.took_place}</span>
-                                </div>
+			{#each blogList as post}
+				<div
+					class="card h-50 w-96 min-w-50 bg-base-100 transition duration-300 ease-in-out outline-solid hover:scale-101"
+				>
+					<a href={`blog_post/${post.id}`} class="h-50 rounded-xl bg-base-100">
+						<figure>
+							<img src={pb.files.getURL(post, post.images[0], { thumb: '400x0' })} alt="For post" />
+						</figure>
+						<div class="card-body rounded-b-xl bg-base-300">
+							<h2 class="card-title">{post.title}</h2>
+							<p>{@html post.description}</p>
+							<hr />
+							<div class="card-actions">
+								<span>Written by: {post.author}</span>
+								<div class="text-right">
+									<span>{post.took_place}</span>
+								</div>
+							</div>
 						</div>
-					</div>
-				</a>
-			</div>
+					</a>
+				</div>
+			{/each}
 		</div>
 	</div>
-    {/each}
 </div>
--- src/routes/blog_post/[id]/+page.svelte
+++ src/routes/blog_post/[id]/+page.svelte
@@ -5,61 +5,85 @@
 	import '../../../app.css';
 	import { pb } from '$lib/pocketbase';
 
-    var postId = page.params.id
-    if (postId == undefined){
-        postId = ""
-    }
-    let post: any;
-    let fail = false;
-    let finished = false;
+	var postId = page.params.id;
+	if (postId == undefined) {
+		postId = '';
+	}
+	let post: any;
+	let fail = false;
+	let finished = false;
 
-
-    onMount(async () => {
-        try {
-            // @ts-ignore
-            post = await pb.collection("posts").getOne(postId)
-            finished = true;
-        } catch (error) {
-            fail = true;
-            finished = true;
-        }
-    })
-
+	onMount(async () => {
+		try {
+			// @ts-ignore
+			post = await pb.collection('posts').getOne(postId);
+			finished = true;
+		} catch (error) {
+			fail = true;
+			finished = true;
+		}
+	});
 </script>
 
 <HCnav></HCnav>
 
 <svelte:head>
-    {#if finished && !fail}
-    <title>HC@NFHS | {post.title}</title>
-    {:else if finished && fail}
-    <title>HC@NFHS | Not Found!</title>
-    {:else if !finished}
-    <title>HC@NFHS | Blog Post</title>
-    {/if}
+	{#if finished && !fail}
+		<title>HC@NFHS | {post.title}</title>
+	{:else if finished && fail}
+		<title>HC@NFHS | Not Found!</title>
+	{:else if !finished}
+		<title>HC@NFHS | Blog Post</title>
+	{/if}
 </svelte:head>
 
-<div class="bg-base-200 min-h-screen pt-15 text-center">
-    
-    {#if finished && !fail}
-        <h1>{post.title}</h1>
-    {:else if finished && fail}
-        <h1>Post Not Found!</h1>
-    <!-- svelte-ignore block_empty -->
-    {:else if !finished}
-        &nbsp;
-    {/if}
+<div class="flex min-h-screen justify-center bg-base-200 pt-20">
+	{#if finished && !fail}
+		<div class="max-w-[50%]">
+			<h1 class="text-3xl">{post.title}</h1>
+			<h2>Written by: {post.author}</h2>
+			<h2>{post.took_place}</h2>
+			<div class="pt-5">
+				<div class="carousel">
+					{#each post.images as image}
+						<div id="item1" class="carousel-item max-h-[50%] w-full">
+							<img
+								src={pb.files.getURL(post, image)}
+								alt={`From ${post.title}`}
+								class="max-h-[75%] rounded-sm"
+							/>
+						</div>
+					{/each}
+				</div>
+				<div>{@html post.body_text}</div>
+			</div>
+		</div>
+	{:else if finished && fail}
+		<h1>Post Not Found!</h1>
+	{:else if !finished}
+		&nbsp;
+	{/if}
 </div>
 
-<div class="content-center justify-center flex">
-  <div class="fixed bottom-0 w-200 justify-center pb-5">
-    {#if fail}
-    <div role="alert" class="alert alert-error" id="submit_error">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-      </svg>
-      <span>Post not found!</span>
-    </div>
-    {/if}
-  </div>
+<div class="flex content-center justify-center">
+	<div class="fixed bottom-0 w-200 justify-center pb-5">
+		{#if fail}
+			<div role="alert" class="alert alert-error" id="submit_error">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6 shrink-0 stroke-current"
+					fill="none"
+					viewBox="0 0 24 24"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+					/>
+				</svg>
+				<span>Post not found!</span>
+			</div>
+		{/if}
+	</div>
 </div>
